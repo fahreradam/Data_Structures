c78251634248bd52e59eab5fc9ebbd95bd67d9c2
fahreradam <fahreradam@gmail.com>
2022-01-12 19:44:24 -0500

    Added the functions for clear, get, find, remove, and remove_all. Have not been tested

--- Lab 1/include/array_list.h
+++ Lab 1/include/array_list.h
@@ -15,9 +15,11 @@ namespace ssuds
 		void append(std::string new_string);
 		unsigned int size();
 		void insert(std::string new_string, unsigned int index);
-		void remove(unsigned int index);
+		void clear();
+		std::string get(unsigned int index);
 		int find(std::string search_value, int start_index = 0);
-
+		void remove(unsigned int index);
+		void remove_all(std::string search_value)
 
 	};
 }
--- Lab 1/src/array_list.cpp
+++ Lab 1/src/array_list.cpp
@@ -60,7 +60,50 @@ void ssuds::ArrayList::insert(std::string new_string, unsigned int index)
 		throw std::out_of_range("index out of range");
 }
 
-//void ssuds::ArrayList::remove(unsigned int index)
-//{
-//	
-//}
+void ssuds::ArrayList::clear()
+{
+	delete[] mData;
+	mData = nullptr;
+	mSize = 0;
+}
+
+std::string ssuds::ArrayList::get(unsigned int index)
+{
+	return mData[index];
+}
+
+int ssuds::ArrayList::find(std::string search_value, int start_index = 0)
+{
+	for (int i = start_index; i < mSize; i++)
+	{
+		if (mData[i] == search_value)
+			return i;
+	}
+	return -1;
+}
+void ssuds::ArrayList::remove(unsigned int index)
+{
+	if (0 <= index || index <= mSize)
+	{
+		std::string* temp_array = new std::string[mSize + 1];
+		for (int i = 0; i < mSize; i++)
+		{
+			if(i != index)
+				temp_array[i] = mData[i];
+		}
+		delete[] mData;
+		mData = temp_array;
+		mSize--;
+	}
+	else
+	{
+		throw std::out_of_range("Index out of range");
+	}
+}
+
+void ssuds::ArrayList::remove_all(std::string search_value)
+{
+	while (find(search_value))
+		remove(find(search_value));
+}
+

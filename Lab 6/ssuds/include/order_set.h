c6071298cbcb0cd5ab704d0665ae29e51c17b5d1
fahreradam <72369125+fahreradam@users.noreply.github.com>
2022-03-21 13:35:24 -0400

    Updated lab 6

--- Lab 6/ssuds/include/order_set.h
+++ Lab 6/ssuds/include/order_set.h
@@ -1,64 +1,207 @@
-#pragma once
-#include <string>
+#include <array_list.h>
 
-namespace {
+namespace ssuds
+{
 	template <class T>
-	class OrderSet 
+	class OrderedSet
 	{
 	protected:
-		class node
+		enum class NodeType {Pre, Post, In_Order};
+		class Node
 		{
+		public:
 			T mData;
-			node* mPrev;
-			node* mRight;
-			node* mLeft;
+			Node* mLeft;
+			Node* mRight;
+			NodeType mType;
 
-			node(T val) : mData(val), mRight(nullptr), mLeft(nullptr), mPrev(nullptr)
+			Node(const T& val) : mData(val), mLeft(nullptr),
+				mRight(nullptr) {}
+
+			int insert_recursive(const T& val)
 			{
-
-			}
-
-			node(T val, node* prev) : mData(val), mPrev(prev), mRight(nullptr), mLeft(nullptr)
-			{
-
-			}
-		
-			void insert(T val, node* start)
-			{
-				node* temp = start;
-				while (temp != nullptr)
+				if (val < mData)
 				{
-					if (val < temp->mData)
+					// The value is less than me, so it should
+					// go to my left.
+					// Two sub-posibilities: 
+					if (mLeft)
 					{
-						if (temp->mLeft == nullptr)
-						{
-							node* new_node = new node(val, temp); // ask jason
-							new_node->mPrev = temp;
-							new_node->mLeft = nullptr;
-							new_node->mRight = nullptr;
-							break;
-						}
+						//    we have a left child -- it should be
+						//       told to handle the rest of insert
+						return mLeft->insert_recursive(val);
 					}
-					
+					else
+					{
+						//    we don't have a left child -- make
+						//       one!
+						mLeft = new Node(val);
+						return 1;
+					}
+				}
+				else if (val > mData)
+				{
+					// The value is greater than me, so it should
+					// go to my right.
+					if (mRight)
+					{
+						return mRight->insert_recursive(val);
+					}
+					else
+					{
+						mRight = new Node(val);
+						return 1;
+					}
+				}
+				else
+				{
+					// The value is equal to me -- duplicate!!
+					// nothing to do...or is there?
+					return 0;
 				}
 			}
 
+			void print_all_recursive()
+			{
+				// If we had the tree on slide 11, we would
+				// get this output
+				// M, G, B, A, D, C, S, P, Q -- pre-order
+				//std::cout << mData << std::endl;
+				if (mLeft)
+					mLeft->print_all_recursive();
+				// If we were to put the cout here...
+				std::cout << mData << std::endl;
+				// ... instead of where it was, we get this:
+				// .......M......
+				// ...G...M..S...
+				// A, BCDGMPSQ -- in-order traversal
+				if (mRight)
+					mRight->print_all_recursive();
+
+
+				// tangent: LISP: (- (+ 3 4) (* 2 3))  => 6
+				//    equiv. to (3 + 4) - (2 * 3)   -- a pre-order language
+			}
+
+			void traversal_recursive(ssuds::ArrayList<T> alist, NodeType tp)
+			{
+				if (tp == NodeType::Pre)
+				{
+					alist.append(mData);
+					if (mLeft)
+						mLeft->traversal_recursive(alist, tp);
+					if (mRight)
+						mRight->traversal_recursive(alist, tp);
+				}
+				if (tp == NodeType::Post)
+				{
+					if (mLeft)
+						mLeft->traversal_recursive(alist, tp);
+					if (mRight)
+						mRight->traversal_recursive(alist, tp);
+					alist.append(mData);
+				}
+				if (tp == NodeType::In_Order);
+				{
+					if (mLeft)
+						mLeft->traversal_recursive(alist, tp);
+
+					alist.append(mData);
+					
+					if (mRight)
+						mRight->traversal_recursive(alist, tp);
+
+				}
+			}
+
+			void clear_recursive()
+			{
+				if (mLeft)
+				{
+					mLeft->clear_recursive();
+					delete mLeft;
+				}
+				if (mRight)
+				{
+					mRight->clear_recursive();
+					delete mRight;
+				}
+			}
+
+			bool contains_recursive(const T& val)
+			{
+				if (mData == val)
+					return true;
+				if (mLeft)
+				{
+					mLeft->clear_recursive();
+				}
+				if (mRight)
+				{
+					mRight->clear_recursive();
+				}
+			}
 		};
 
-	protected:
-		node* mStart;
-		node* mEnd;
-		unsigned int mSize;
-	
-	public:
-		OrderSet() : mSize(0), mStart(nullptr), mEnd(nullptr)
-		{
+		Node* mRoot;
+		int mSize;
 
+	public:
+		OrderedSet() : mRoot(nullptr), mSize(0) {}
+
+		~OrderedSet()
+		{
+			// super-important!!
+			clear();
 		}
 
-		void insert(T val)
+		// The user is calling this method
+		void insert(const T& val)
 		{
-			
+			if (mRoot)
+			{
+				// We have elements already -- tell the root
+				// to handle it!
+				mSize += mRoot->insert_recursive(val);
+			}
+			else
+			{
+				mRoot = new Node(val);
+				mSize = 1;
+			}
+		}
+
+		void traversal(ssuds::ArrayList<T> alist, NodeType tp)
+		{
+			if (mRoot)
+			{
+				mRoot->traversal_recursive(alist, tp);
+			}
+		}
+
+
+		void print_all()
+		{
+			if (mRoot)
+				mRoot->print_all_recursive();
+		}
+
+		int size()
+		{
+			return mSize;
+		}
+
+		void clear()
+		{
+			mRoot->clear_recursive();
+			delete mRoot;
+			mRoot = nullptr;
+			mSize = 0
+		}
+
+		bool contains(const T& val)
+		{
+			return mRoot->contains_recursive(val);
 		}
 	};
 }
--- Lab 6/ssuds/src/main.cpp
+++ Lab 6/ssuds/src/main.cpp
@@ -6,21 +6,13 @@
 
 int main()
 {
-    // My temporary tests (I eventually did this in google-tests in linked_list_tests.cpp)
-    ssuds::LinkedList<std::string> slist;
-    slist.append("Abe");
-    slist.append("Bob");
-    slist.append("Carl");
-    std::cout << "slist = " << slist << std::endl;
-    ssuds::LinkedList<std::string>::LinkedListIterator iter = slist.begin();
-    while (iter != slist.end())
-    {
-        std::string cur = *iter;
-        std::cout << cur << std::endl;
-        ++iter;
-    }
+    //testing::InitGoogleTest();
+    //RUN_ALL_TESTS();
 
-    // Invoke all google test fixtures we've registered
-    testing::InitGoogleTest();
-    RUN_ALL_TESTS();
+    ssuds::OrderedSet<float> S;
+    S.insert(3.4f);
+    S.insert(5.8f);
+    S.insert(4.2f);
+    S.insert(5.8f);
+    std::cout << S.size();		// 3
 }

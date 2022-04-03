80ef7f2535224c85c32546c6d4fbddb17535abde
Adam Fahrer <fahreradam@gmail.com>
2022-04-03 17:05:34 -0400

    fhjdes

---
+++ Lab 7/ssuds/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <linked_list.h>
+#include <gtest/gtest.h>
+#include <order_set.h>
+#include <unordered_map.h>
+
+int main()
+{
+    testing::InitGoogleTest();
+    RUN_ALL_TESTS();
+}

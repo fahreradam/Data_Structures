d66f70e01c866e506c0a2121b7771a4285bdc6f8
fahreradam <72369125+fahreradam@users.noreply.github.com>
2022-04-11 12:36:52 -0400

    ,

--- Lab 8/ssuds/src/level.cpp
+++ Lab 8/ssuds/src/level.cpp
@@ -1,5 +1,11 @@
 #include <level.h>
 #include <fstream>
+
+Level::Level()
+{
+	std::map<std::string, std::map<std::string, float>> the_graph;
+}
+
 void Level::load_map_file(std::string fname)
 {
 	std::ifstream fp;
@@ -7,15 +13,14 @@ void Level::load_map_file(std::string fname)
 
 	if (fp.is_open())
 	{
-		std::string line_type, name;
-		int x, y, radius;
+		std::string line_type;
+		LevelNode temp;
 
 		while (!fp.eof())
 		{
 			if (line_type == "n")
 			{
-				fp >> name >> x >> y >> radius;
-				
+				fp >> temp.mName >> temp.mX >> temp.mY >> temp.mRadius;
 			}
 		}
 	}
